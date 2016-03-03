8627cb939793c8f6b68e3f938e42cfc200b3299b
wyx111 <2538066216@qq.com>
2016-03-03 16:50:44 +0800

    3.添加JsonUtils和Fabric

--- app/src/main/java/duoy/cn/meplusclienttest/utils/JsonUtils.java
+++ app/src/main/java/duoy/cn/meplusclienttest/utils/JsonUtils.java
@@ -1,8 +1,35 @@
 package duoy.cn.meplusclienttest.utils;
 
+import android.util.Log;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
 /**
  * Created by Dream on 2016/3/3.
  */
 public class JsonUtils {
+    private static final String TAG = JsonUtils.class.getSimpleName();
+    static ObjectMapper mapper= new ObjectMapper();//// can reuse, share globally,ObjectMapper是jackson中的主要类
+
+    public static <T> T readValue(String content, Class<T> valueType) {//把json转成对象
+        try {
+            return mapper.readValue(content, valueType);
+        } catch (IOException e) {
+            Log.e(TAG, "readValue", e);
+        }
+        return null;
+    }
+
+    public static String writeValueAsString(Object obj) {//把对象转成json
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            Log.e(TAG, "writeValueAsString", e);
+        }
+        return null;
+    }
 
 }
